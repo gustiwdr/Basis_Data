54f411bc2b561569e6c3d5ce04d26b7d0b2eea7f
Gusti Putu W <139354034+gustiwdr@users.noreply.github.com>
2023-11-16 17:30:16 +0700

    Update Jawaban2.sql

--- UTS/Jawaban2.sql
+++ UTS/Jawaban2.sql
@@ -6,14 +6,14 @@ VALUES ("CR041", "Basis Data", 3, "Teknik Informatika"),
 
 INSERT INTO mahasiswa
 VALUES	(20220801124, "Gusti Putu", "Wulandari", "2004-09-14", "Teknik Informatika", "CR041"),
-				(20220801148, "Aulia Fitri", "Nur Cahyati", "2003-11-15", "Teknik Informatika", "CR041"),
-				(20220801329, "Yunus Cornelis", "Kabe", "2001-07-19", "Teknik Informatika", "CR041"),
-				(20220703041, "Nadia Recilia", "Dahmi", "2004-05-21", "Ilmu Keperawatan", "CR056"),
-				(20220703001, "Ellena", "Valentina", "2004-09-09", "Ilmu Keperawatan", "CR056"),
-				(20220103032, "Meisya Margareta", "Yovita", "2002-05-27", "PGSD", "CR023");
+	(20220801148, "Aulia Fitri", "Nur Cahyati", "2003-11-15", "Teknik Informatika", "CR041"),
+	(20220801329, "Yunus Cornelis", "Kabe", "2001-07-19", "Teknik Informatika", "CR041"),
+	(20220703041, "Nadia Recilia", "Dahmi", "2004-05-21", "Ilmu Keperawatan", "CR056"),
+	(20220703001, "Ellena", "Valentina", "2004-09-09", "Ilmu Keperawatan", "CR056"),
+	(20220103032, "Meisya Margareta", "Yovita", "2002-05-27", "PGSD", "CR023");
 
 
 INSERT INTO dosen
-VALUES (7865, "Jason", "Corbuzier", "CR041"),
-			 (7356, "Riri", "Riza", "CR056"),
-			 (7653, "Dede", "Taulany", "CR023");
+VALUES 	(7865, "Jason", "Corbuzier", "CR041"),
+	(7356, "Riri", "Riza", "CR056"),
+	(7653, "Dede", "Taulany", "CR023");
